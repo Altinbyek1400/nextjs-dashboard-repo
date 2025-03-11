86ac12658f4b5c38d9d1bc058579f5bad358f6b5
Altanbyek <altinbekaibyek@gmail.com>
2025-03-11 15:17:10 +0800

    test

--- app/query/route.ts
+++ app/query/route.ts
@@ -7,19 +7,15 @@ async function listInvoices() {
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
   `;
   return data;
 }
 
 export async function GET() {
-  // return Response.json({
-  //   message:
-  //     "Uncomment this file and remove this line. You can delete this file when you are finished.",
-  // });
   try {
     return Response.json(await listInvoices());
   } catch (error) {
+    console.error("Database Query Error:", error);
     return Response.json({ error }, { status: 500 });
   }
 }
